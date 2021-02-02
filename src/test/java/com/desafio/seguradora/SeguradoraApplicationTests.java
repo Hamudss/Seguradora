a849e12b8ab8f2b466fc9e091dffc8e2b15fb1c1
Hamud de Souza <hamudss@hotmail.com>
2021-02-02 20:12:26 -0300

    initial commit

---
+++ src/test/java/com/desafio/seguradora/SeguradoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.seguradora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguradoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
